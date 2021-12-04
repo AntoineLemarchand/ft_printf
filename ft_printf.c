1c947d224992bfe47209d7db1d89b8c2af0c011c
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2021-12-04 15:00:57 +0100

    basic idea done

---
+++ ft_printf.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 22:09:05 by alemarch          #+#    #+#             */
+/*   Updated: 2021/12/04 15:00:40 by antoine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libftprintf.h"
+
+static int	ft_isconv(char c)
+{
+	return (c == 'c'
+		|| c == 's'
+		|| c == 'p'
+		|| c == 'd'
+		|| c == 'i'
+		|| c == 'u'
+		|| c == 'x'
+		|| c == 'X'
+		|| c == '%');
+}
+
+static t_format	ft_init_format(int reload)
+{
+	t_format	format;
+
+	format.plus = 0;
+	format.minus = 0;
+	format.dot = 0;
+	format.hash = 0;
+	format.space = 0;
+	format.minwidth = 0;
+	if (reload)
+		format.size = 0;
+	return (format);
+}
+
+static int	ft_load_format(char *str, t_format format, int index)
+{
+	while (!ft_iconv(str[index]))
+	{
+		if (str[index] == '+')
+			format.plus = 1;
+		else if (str[index] == '-')
+			format.minus = 1;
+		else if (str[index] == '.')
+			format.dot = 1;
+		else if (str[index] == '#')
+			format.hash = 1;
+		else if (str[index] == ' ')
+			format.space = 1;
+		else if (ft_isdigit(str[index]))
+			format.minwidth = ft_atoi(str[index]);
+		index += 1;
+	}
+	return (index);
+}
+
+static int	ft_managearg(char *str, va_list args, int index)
+{
+	t_format	format;
+
+	format = ft_init_format(0);
+	index = ft_load_format(str, format, index);
+	if (str[index] == 'c')
+		format.size += ft_manage_char(format, args);
+	else if (str[index] == 's')
+		format.size += ft_manage_str(format, args);
+	else if (str[index] == 'p')
+		format.size += ft_manage_addr(format, args);
+	else if (str[index] == 'd' || str[index] == 'i')
+		format.size += ft_manage_nbr(format, args);
+	else if (str[index] == 'u')
+		format.size += ft_manage_unsigned(format, args);
+	else if (str[index] == 'x')
+		format.size += ft_manage_hex(format, args, 0);
+	else if (str[index] == 'X')
+		format.size += ft_manage_hex(format, args, 1);
+	else if (str[index] == '%')
+		format.size += write(1, '%' 1);
+	index++;
+	return (index);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	t_format	print;
+	va_list		args;
+	int			i;
+
+	size = 0;
+	print = ft_init_struct(1);
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+			i = ft_managearg(str, args, i + 1);
+		else
+		{
+			write(1, str[i++], 1);
+			print.size += 1;
+		}
+	}
+	va_end(args);
+	return (print.size);
+}
--- libftprintf.h
+++ libftprintf.h
@@ -1,15 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 22:08:36 by alemarch          #+#    #+#             */
-/*   Updated: 2021/11/24 22:08:59 by alemarch         ###   ########.fr       */
+/*   Updated: 2021/12/04 14:35:08 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include<unistd.h>
+# include<stddef.h>
+# include<stdlib.h>
+
+typedef struct s_format {
+	int		plus;
+	int		minus;
+	int		zero;
+	int		dot;
+	int		hash;
+	int		space;
+	size_t	minwidth;
+}	t_format;
 #endif
